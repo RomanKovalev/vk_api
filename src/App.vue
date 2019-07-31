fb27526a8fc5f1c85e4e6386788db53f8a926a60
Roman Kovalev <3217548+RomanKovalev@users.noreply.github.com@users.noreply.github.com>
2019-07-31 23:09:23 +0300

    Added markup

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,47 @@
   <v-app>
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>API VK EXAMPLE</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
+      <span></span>
+      <v-btn text>
+        <span class="mr-2">Login</span>
+      </v-btn>
+      <v-btn text>
+        <span class="mr-2">Logout</span>
       </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <v-item-group>
+        <v-container grid-list-md>
+          <h2 class="font-weight-regular font-italic">6 friends of Ivan Ivanov</h2>
+          <v-layout wrap>
+            <v-flex v-for="n in 6" :key="n" xs12 md4>
+              <v-item>
+                <v-card class="flex">
+                <v-card-title>Ivan petrov</v-card-title>
+                  <v-img
+                    :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+                    aspect-ratio="1"
+                    class="grey lighten-2"
+                  >
+                  </v-img>
+                </v-card>
+              </v-item>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-item-group>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-  data: () => ({
-    //
-  }),
+  name: "App",
+  components: {},
+  data: () => ({})
 };
 </script>
